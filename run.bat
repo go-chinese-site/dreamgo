4fb3f0e00e7d18651e5f3e7b9829a138786db8cb
ericlibinbin <eric_libinbin@l63.com>
2017-10-12 19:18:22 +0800

    update run.bat file

--- run.bat
+++ run.bat
@@ -6,20 +6,38 @@ if exist run.bat goto exec
  echo run.bat must be run from its folder
 
 :exec
-tasklist /nh|find /i "dreamgo.exe"
+tasklist /nh|find /i "dreamgo"
 
-if ERRORLEVEL 1 goto reload else(
-    goto stop
+if ERRORLEVEL 1 (
     goto reload
-    )
+ ) else (
+     goto kill
+ )
+
+
 
 :reload
 call install.bat
-if exist bin\dreamgo.exe go run else echo install fail...
+if exist bin\dreamgo.exe (
+    echo .........rebuild success.........
+    goto run
+    )else (
+    echo .........the command install fail.........
+    goto end
+    )
 
 :run
-echo running success...
-bin\dreamgo.exe
+echo .........the program is starting.......
+start /min bin\dreamgo.exe
+echo .........started success.........
+goto end
 
-:stop
- taskkill /im "dreamgo.exe"
+:kill
+echo .........run kill exist process.........
+ taskkill /f /im "dreamgo.exe" /t >null 2>&1
+ goto reload
+
+:end
+echo run finished
+
+ 
