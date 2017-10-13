7c52c0d06fe6144a414df963b3dcbb3cc86a9b1d
eric_libinbin <eric_libinbin@163.com>
2017-10-13 10:42:43 +0800

    update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-set -e
+#set -e
 
 if [ ! -f run.sh ]; then
 	echo 'install must be run within its container folder' 1>&2
@@ -9,7 +9,7 @@ fi
 
 ps -ef|grep dreamgo |grep -v grep
 if [$? -ne 0];then
-    source install.sh
+    source ./install.sh
     if [! -f bin/dreamgo.exe];then
      echo .........the command install fail.........
      exit 1
@@ -22,7 +22,7 @@ if [$? -ne 0];then
 else
     echo .........run kill exist process.........
     ps -ef|grep dreamgo |grep -v grep |awk '{print $2}' |xargs kill -9 >/dev/null 2>&1
-     source install.sh
+     source ./install.sh
     if [! -f bin/dreamgo.exe];then
      echo .........the command install fail.........
      exit 1
