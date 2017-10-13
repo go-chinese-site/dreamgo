8614e4e3fc37ced7fd995006670a6c6d00df33d5
HMSJ32\linweicong <2092598706@qq.com>
2017-10-13 10:01:58 +0800

    同步上游仓库

---
+++ run.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+set -e
+
+if [ ! -f run.sh ]; then
+	echo 'install must be run within its container folder' 1>&2
+	exit 1
+fi
+
+ps -ef|grep dreamgo |grep -v grep
+if [$? -ne 0];then
+    source install.bat
+    if [! -f bin/dreamgo.exe];then
+     echo .........the command install fail.........
+     exit 1
+     else 
+     echo .........the program is starting.......
+     nohup ./bin/dreamgo & >/dev/null 2>&1
+     echo .........started success.........
+     echo finished
+     fi
+else
+    echo .........run kill exist process.........
+    ps -ef|grep dreamgo |grep -v grep |awk '{print $2}' |xargs kill -9 >/dev/null 2>&1
+     source install.bat
+    if [! -f bin/dreamgo.exe];then
+     echo .........the command install fail.........
+     exit 1
+     else 
+     echo .........the program is starting.......
+     nohup ./bin/dreamgo & >/dev/null 2>&1
+     echo .........started success.........
+     echo finished
+fi
+
+
+
+ 
