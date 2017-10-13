240d1319cef61e0e6a85992ee70a5dbd61e88cb7
eric_libinbin <eric_libinbin@163.com>
2017-10-13 10:57:47 +0800

    update run.sh

--- run.sh
+++ run.sh
@@ -8,14 +8,14 @@ if [ ! -f run.sh ]; then
 fi
 
 ps -ef|grep dreamgo |grep -v grep
-if [$? -ne 0];then
+if [ $? -ne 0 ];then
     source ./install.sh
-    if [! -f bin/dreamgo.exe];then
+    if [ ! -f bin/dreamgo ];then
      echo .........the command install fail.........
      exit 1
      else 
      echo .........the program is starting.......
-     nohup ./bin/dreamgo & >/dev/null 2>&1
+     nohup ./bin/dreamgo >/dev/null 2>&1 &
      echo .........started success.........
      echo finished
      fi
@@ -23,13 +23,13 @@ else
     echo .........run kill exist process.........
     ps -ef|grep dreamgo |grep -v grep |awk '{print $2}' |xargs kill -9 >/dev/null 2>&1
      source ./install.sh
-    if [! -f bin/dreamgo.exe];then
+    if [ ! -f bin/dreamgo ];then
      echo .........the command install fail.........
      exit 1
      else 
      echo .........the program is starting.......
-     nohup ./bin/dreamgo & >/dev/null 2>&1
+     nohup ./bin/dreamgo >/dev/null 2>&1 &
      echo .........started success.........
-     echo finished
+     echo start finished
      fi
 fi
